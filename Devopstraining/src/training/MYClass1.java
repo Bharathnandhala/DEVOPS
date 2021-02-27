9dd7c61dd39d406b5a30f4e7125db6f51d8491fd
bhara <bhara@MSI>
2021-02-27 07:54:56 +0530

    added new class and add two numbers.

--- Devopstraining/src/training/MYClass1.java
+++ Devopstraining/src/training/MYClass1.java
@@ -7,6 +7,8 @@ public class MYClass1 {
 System.out.println("hello ");
 int a=6;
 int b=10;
+int c=a=b;
+System.out.println("c value is"+c);
 
 	}
 
---
+++ Devopstraining/src/training/class2.java
@@ -0,0 +1,9 @@
+package training;
+
+public class class2 {
+	
+	public static void main(String[] args) {
+		System.out.println("This is 2 nd program");
+	}
+
+}
